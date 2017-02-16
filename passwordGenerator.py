282da7cab9740708d7757203f6e94f55ffbfb8ab
Adriano Maia <adriano.r.r.f.m@hotmail.com>
2017-02-16 01:44:19 +0000

    exercice eight

---
+++ passwordGenerator.py
@@ -0,0 +1,33 @@
+'''
+Write a password generator in Python.
+Be creative with how you generate passwords - strong passwords have a mix of lowercase letters, uppercase letters, numbers, and symbols.
+The passwords should be random, generating a new password every time the user asks for a new password.
+Include your run-time code in a main method.
+
+Extra:
+    --> Ask the user how strong they want their password to be.
+    For weak passwords, pick a word or two from a list.
+'''
+
+import random,string
+
+def lenPass():
+    try:
+        passleng = int(input("Define a length for our password (at least equal to four): "))
+    except Exception as e:
+        print("You should type a integer number")
+    if passleng < 4 or passleng > 20: # I choose four and ten, you can choose your own number
+        print("You have to choose a number between 4 and 20")
+    else:
+        return passleng
+
+# now lets make a random password using the module string and ASCII characters
+
+def randomPass():
+    passleng = lenPass()
+    chars = string.ascii_uppercase + string.digits + string.ascii_lowercase
+    password = ''.join(random.SystemRandom().choice(chars) for _ in range(passleng))
+    # use SystemRandom() for cryptographically security
+    return password
+
+randomPass()
